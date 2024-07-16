6b19324c1a3f1be8fb5c682be7d313b11d413b53
20221169 <gs20221169@office.kopo.ac.kr>
2024-07-16 10:42:57 +0900

    Implement add method

--- src/src/main/java/com/example/demo/SimpleBoardController.java
+++ src/src/main/java/com/example/demo/SimpleBoardController.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,8 +13,24 @@ import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class SimpleBoardController {
+	@Autowired
+	private SimpleBoardDataRepository simpleBoardDAO;
+	
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String index(HttpServletRequest request) {
 		return "index";
 	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public String add(HttpServletRequest request) {
+		String name = request.getParameter("username");
+		String contents = request.getParameter("contents");
+		SimpleBoardData simpleBoard = SimpleBoardData.builder()
+						.name(name)
+						.contents(contents)
+						.build();
+		simpleBoardDAO.save(simpleBoard);
+		return "index";
+	}
+	
 }
