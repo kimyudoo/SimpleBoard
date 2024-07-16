21a6b53410fa0a63660beddc7d51df3a4547c2ed
20221169 <gs20221169@office.kopo.ac.kr>
2024-07-16 09:33:43 +0900

    Insert database code

---
+++ src/src/main/java/com/example/demo/SimpleBoardData.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@Getter
+@RequiredArgsConstructor
+@Table(name = "tbl_simpleboard")
+@Entity
+@Builder
+public class SimpleBoardData {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //MySQL의 AUTO_INCREMENT를 사용    
+	private Long id;   
+	
+	@Column(length = 50, nullable = false)    
+	private String name;
+	
+	@Column(length = 200, nullable = false)    
+	private String contents; 	
+			
+	public SimpleBoardData(Long id, String name, String contents) {
+		this.id = id;
+		this.name = name;
+		this.contents = contents;
+	}
+}
